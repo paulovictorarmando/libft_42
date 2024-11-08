7f1e12097480d078ca425a17ba07e2b33ebbc5b5
Paulo Armando <122297062+paulovictorarmando@users.noreply.github.com>
2024-11-08 17:11:17 -0500

    Add files via upload
    add all files

---
+++ ft_lstadd_back.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parmando <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 13:44:12 by parmando          #+#    #+#             */
+/*   Updated: 2024/05/20 17:32:41 by parmando         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+	}
+	else
+	{
+		temp = *lst;
+		while (temp->next)
+			temp = temp -> next;
+		temp -> next = new;
+	}
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parmando <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 12:25:47 by parmando          #+#    #+#             */
+/*   Updated: 2024/05/20 12:38:16 by parmando         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*e;
+
+	e = malloc(sizeof(t_list));
+	if (!e)
+		return (NULL);
+	e->content = content;
+	e->next = NULL;
+	return (e);
+}
